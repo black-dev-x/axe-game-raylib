ae3946e9b003af8ceb042608bd65627461df4a4d
black-dev-x <thiago.dev.x@gmail.com>
2024-05-20 15:33:38 -0300

    update

--- axe_game.cpp
+++ axe_game.cpp
@@ -4,8 +4,10 @@ int main()
 {
   int width = 350;
   int height = 200;
-  float root_beer = 1.99;
-  double cheese_burger = 5.99;
-  bool shouldHaveLunch = true;
   InitWindow(width, height, "oh well");
+  while(true){
+    BeginDrawing();
+    ClearBackground(WHITE);
+    EndDrawing();
+  }
 }
--- bools.cpp
+++
@@ -1,9 +0,0 @@
-int main()
-{
-  bool equal{4 == 9};
-  bool notEqual{4 != 9};
-  bool less{4 < 9};
-  bool greater{4 > 9};
-  bool lessEqual{4 <= 9};
-  bool greaterEqual{4 >= 9};
-}
