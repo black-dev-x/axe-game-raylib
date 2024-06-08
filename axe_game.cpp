f57aa5df6040a76aa6fbc86ab805f026053c877d
black-dev-x <thiago.dev.x@gmail.com>
2024-06-08 19:19:10 -0300

    circle moving

--- axe_game.cpp
+++ axe_game.cpp
@@ -2,12 +2,38 @@
 
 int main()
 {
+  // window dimensions
   int width = 350;
   int height = 200;
+
+  // c
+  int circleX = 0;
+  int circleY = 0;
+  int speed = 1;
+
   InitWindow(width, height, "oh well");
-  while(true){
+  
+  SetTargetFPS(60);
+  while(!WindowShouldClose()){
     BeginDrawing();
     ClearBackground(WHITE);
+    if(IsKeyDown(KEY_D))
+    {
+      circleX += speed;
+    } 
+    else if(IsKeyDown(KEY_A))
+    {
+      circleX -= speed;
+    }
+    if(IsKeyDown(KEY_W))
+    {
+      circleY -= speed;
+    }
+    else if(IsKeyDown(KEY_S))
+    {
+      circleY += speed;
+    }
+    DrawCircle(circleX, circleY, 50, BLUE);
     EndDrawing();
   }
 }
