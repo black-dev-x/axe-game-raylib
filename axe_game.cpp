b087ec3ca4e845ac1090a4f4a5979567ac9633f8
ThiagoCoderfull <thiago.dev.x@gmail.com>
2024-09-28 12:14:06 -0300

    Update circle speed and add collision detection

--- axe_game.cpp
+++ axe_game.cpp
@@ -10,7 +10,7 @@ int main()
   int circleX = 200;
   int circleY = 200;
   int circleRadius = 25;
-  int circleSpeed = 1;
+  int circleSpeed = 10;
 
   // rectangle
   int rectangleWidth = 50;
@@ -19,37 +19,48 @@ int main()
   int rectangleX = 300;
   int rectangleSpeed = 10;
 
+  bool collision_with_axe = false;
+
   InitWindow(width, height, "Axe Game");
   
   SetTargetFPS(60);
   while(!WindowShouldClose()){
     BeginDrawing();
     ClearBackground(WHITE);
-    DrawCircle(circleX, circleY, circleRadius, BLUE);
-    DrawRectangle(rectangleX, rectangleY, rectangleWidth, rectangleHeight, RED);
+    if(collision_with_axe)
+    {
+      DrawText("Game Over", width/2 - 50, height/2, 20, RED);
+    } else {
+      DrawCircle(circleX, circleY, circleRadius, BLUE);
+      DrawRectangle(rectangleX, rectangleY, rectangleWidth, rectangleHeight, RED);
+      rectangleY += rectangleSpeed;
+      if(rectangleY > height - rectangleHeight || rectangleY < 0)
+      {
+        rectangleSpeed *= -1;
+      }
+      if(IsKeyDown(KEY_D) && circleX < width - circleRadius)
+      {
+        circleX += circleSpeed;
+      } 
+      else if(IsKeyDown(KEY_A) && circleX > circleRadius)
+      {
+        circleX -= circleSpeed;
+      }
+      if(IsKeyDown(KEY_W) && circleY > circleRadius)
+      {
+        circleY -= circleSpeed;
+      }
+      else if(IsKeyDown(KEY_S) && circleY < height - circleRadius)
+      {
+        circleY += circleSpeed;
+      }
+      //if the circle and rectangle collide
+      if(circleX + circleRadius > rectangleX && circleX - circleRadius < rectangleX + rectangleWidth && circleY + circleRadius > rectangleY && circleY - circleRadius < rectangleY + rectangleHeight)
+      {
+        collision_with_axe = true;
+      }
 
-    rectangleY += rectangleSpeed;
-    if(rectangleY > height - rectangleHeight || rectangleY < 0)
-    {
-      rectangleSpeed *= -1;
     }
-    if(IsKeyDown(KEY_D) && circleX < width - circleRadius)
-    {
-      circleX += circleSpeed;
-    } 
-    else if(IsKeyDown(KEY_A) && circleX > circleRadius)
-    {
-      circleX -= circleSpeed;
-    }
-    if(IsKeyDown(KEY_W) && circleY > circleRadius)
-    {
-      circleY -= circleSpeed;
-    }
-    else if(IsKeyDown(KEY_S) && circleY < height - circleRadius)
-    {
-      circleY += circleSpeed;
-    }
-    
     EndDrawing();
   }
 }
